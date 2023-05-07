8e61686b226e97fef7780b0ed25ff2f86cd2dfa5
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-05-07 02:11:33 +0200

    Login function in Repository

--- lib/data/repository/note_repository.dart
+++ lib/data/repository/note_repository.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:notes_app_with_php/core/error_handling/failures/remote_failures.dart';
 import 'package:notes_app_with_php/data/data_sources/remote/api_services.dart';
+import 'package:notes_app_with_php/data/models/auth/sign_in_model.dart';
 import 'package:notes_app_with_php/data/models/auth/sign_up_model.dart';
 
 import '../../core/error_handling/exceptions/remote_exceptions.dart';
@@ -26,5 +27,24 @@ class NoteRepository {
     }on TimeOutCustomException catch(e){
       return Left(TimeOutFailure(e.message));
     }
+  }
+
+  Future<Either<Failure , void>> login(SignInModel signInModel) async {
+    try {
+      return Right(await apiServices.login(signInModel));
+    }on NoInternetException catch(e){
+      return Left(NoInternetFailure(e.message));
+    }on DataParsingException catch(e){
+      return Left(DataParsingFailure(e.message));
+    }on ServerException catch(e){
+      return Left(ServerFailure(e.message));
+    }on ClientException catch(e){
+      return Left(ClientFailure(e.message));
+    }on ResponseException catch(e){
+      return Left(ResponseFailure(e.message));
+    }on TimeOutCustomException catch(e){
+      return Left(TimeOutFailure(e.message));
+    }
+
   }
 }
